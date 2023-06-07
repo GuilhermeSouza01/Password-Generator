ba82ce79f702c2820eba56fba85c05216512931f
guilhermesouza01 <ghostgui60@gmail.com>
2023-06-07 18:42:38 -0300

    Updating the safe bar

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const simpleCopyButton = document.querySelector("#simple-copy-pass");
 const upperCaseCheckEl = document.querySelector("#uppercase-check");
 const numberCheckEl = document.querySelector("#number-check");
 const symbolCheckEl = document.querySelector("#symbol-check");
+const securityBarEl = document.querySelector("#indicator-bar");
 
 let rangeValue = 16;
 
@@ -33,6 +34,20 @@ function generatePass() {
     password += chars.substring(randomNumber, randomNumber + 1);
   }
   passInput.value = password;
+  qualityOfPassword();
+}
+
+function qualityOfPassword() {
+  //criando pesos para seleções de checkbox
+  //T * 0.25 + M * 0.15 + N * 0.25 + S * 0.25 = 100%
+  const percent = Math.round(
+    (rangeValue / 64) * 25 +
+      (upperCaseCheckEl.checked ? 20 : 0) +
+      (numberCheckEl.checked ? 20 : 0) +
+      (symbolCheckEl.checked ? 25 : 0)
+  );
+  console.log(percent);
+  securityBarEl.style.width = `${percent}%`;
 }
 
 const copyPass = () => {
